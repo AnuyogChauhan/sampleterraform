823e3fedb1fa1858d70f23f79e74245766e20dab
AnuyogChauhan <anuyog.chauhan@gmail.com>
2017-05-29 15:24:30 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,7 +14,9 @@ variable "ssh_user_name" {
   default = "ubuntu"
 }
 
-variable "external_gateway" {}
+variable "external_gateway" {
+  default = "09dcce3e-906d-4afa-8b90-ac04593a0f5d"
+  }
 
 variable "pool" {
   default = "external"
@@ -24,8 +26,14 @@ variable "ansible_script" {
   default = "nginx.yml"
 }
 
-variable "http_proxy" {}
+variable "http_proxy" {
+  default = "http://165.225.104.34:80"
+  }
 
-variable "https_proxy" {}
+variable "https_proxy" {
+  default = "https://165.225.104.34:80"
+  }
 
-variable "no_proxy" {}
+variable "no_proxy" {
+  default = "localhost,127.0.0.1,15.*,16.*,172*"
+  }
