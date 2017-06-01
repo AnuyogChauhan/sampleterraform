b4a7a80b5f2ccedfec91ac726775030c20420426
AnuyogChauhan <anuyog.chauhan@gmail.com>
2017-06-01 13:31:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "openstack" {
 }
 
 resource "openstack_compute_keypair_v2" "terraform_keypair" {
-  name       = "terraform"
+  name       = "terraform_keypair"
   public_key = "${file("${var.ssh_key_file}.pub")}"
 }
 
@@ -18,12 +18,12 @@ resource "openstack_compute_floatingip_v2" "terraform_floatingIP" {
 }
 
 resource "openstack_compute_instance_v2" "terraform_instance" {
-  name            = "terraform"
+  name            = "terraform_instance"
   image_name      = "${var.image}"
   flavor_name     = "${var.flavor}"
-  key_pair        = "${openstack_compute_keypair_v2.terraform.name}"
+  key_pair        = "${openstack_compute_keypair_v2.terraform_keypair.name}"
   security_groups = ["default"]
-  floating_ip     = "${openstack_compute_floatingip_v2.terraform.address}"
+  floating_ip     = "${openstack_compute_floatingip_v2.terraform_floatingIP.address}"
 
   network {
     uuid = "cb3abed9-5fed-4797-a759-f3bbef7846be"
